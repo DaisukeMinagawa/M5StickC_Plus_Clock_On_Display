89079163d17b13516e0c0399bfbe1c4c58be9126
DaisukeMinagawa <iamworkingwell@gmail.com>
2024-08-21 09:11:16 +0900

    This code is fork of https://karakuri-musha.com/inside-technology/arduino-m5stickc-plus-04-clock-on-display/

---
+++ .pio/build/m5stick-c/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/m5stick-c/FrameworkArduino/cbuf.cpp.o: \
+ C:/Users/aki_f/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ C:/Users/aki_f/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
---
+++ .pio/build/m5stick-c/lib041/M5StickCPlus/Fonts/Font72rle.c.d
@@ -0,0 +1,3 @@
+.pio/build/m5stick-c/lib041/M5StickCPlus/Fonts/Font72rle.c.o: \
+ .pio/libdeps/m5stick-c/M5StickCPlus/src/Fonts/Font72rle.c \
+ C:/Users/aki_f/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
